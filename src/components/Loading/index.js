a562f065df466105a1b3b7c27619494ef8ba2b09
wangfanghua618 <wangfanghua618@jk.cn>
2018-11-28 09:53:32 +0800

    Loading组件完成

---
+++ src/components/Loading/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import styles from './index.scss';
+
+function Loading(props) {
+    return (
+        <div className={styles.root}>
+            {props.content || '数据加载中...'}
+        </div>
+    )
+}
+
+Loading.mount = function(options) {
+    let _instance = getInstance(options);
+
+    return _instance
+};
+
+Loading.unmount = function() {
+    ReactDOM.unmountComponentAtNode(container);
+    instance = null;
+}
+
+let container = null;
+let instance = null;
+
+function getInstance(options) {
+    if (!instance) {
+        if (!container) {
+            container = document.createElement('div');
+            document.body.appendChild(container);
+        }
+
+        instance = ReactDOM.render(<Loading {...options} />, container);
+    }
+
+    return instance;
+}
